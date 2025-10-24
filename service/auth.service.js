ab90950ec435c4010b974ea48a75563567badc96
asadbek <asadbekkhudayberdiyev07@gmail.com>
2025-10-25 00:06:56 +0900

    login, logout and refresh

--- service/auth.service.js
+++ service/auth.service.js
@@ -36,6 +36,49 @@ class AuthService {
     user.isActivated = true;
     await user.save();
   }
+  async login(email, password) {
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      throw new Error(`User is not found`);
+    }
+    const isPassword = await bcrypt.compare(password, user.password);
+    if (!isPassword) {
+      throw new Error(`PASSWORD is INCORRECT`);
+    }
+
+    const userDto = new UserDto(user);
+    const tokens = tokenService.generateToken({ ...userDto });
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+    return { user: userDto, ...tokens };
+  }
+
+  async logout(refreshToken) {
+    return await tokenService.removeToken(refreshToken);
+  }
+
+  async refresh(refreshToken) {
+    if (!refreshToken) {
+      throw new Error("Bad authorization");
+    }
+
+    const userPayload = tokenService.validateRefreshToken(refreshToken);
+    console.log("userPayload", userPayload);
+
+    const tokenDb = await tokenService.findToken(refreshToken);
+
+    if (!userPayload || !tokenDb) {
+      throw new Error("Bad authorization");
+    }
+
+    const user = await userModel.findById(userPayload.id);
+    const userDto = new UserDto(user);
+
+    const tokens = tokenService.generateToken({ ...userDto });
+    await tokenService.saveToken(userDto.id, tokens.refreshToken);
+
+    return { user: userDto, ...tokens };
+  }
 }
 
 module.exports = new AuthService();
