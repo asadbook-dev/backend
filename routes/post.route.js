a370c54661d8195cb53eb3ad7ca667e1cbf33b69
asadbek <asadbekkhudayberdiyev07@gmail.com>
2025-10-26 02:47:18 +0900

    Author middleware

--- routes/post.route.js
+++ routes/post.route.js
@@ -1,12 +1,19 @@
 const expres = require("express");
 const postController = require("../controllers/post.controller");
 const logger = require("../middlewares/logger");
+const authMiddleware = require("../middlewares/auth.middleware");
+const authorMiddleware = require("../middlewares/author.middleware");
 
 const router = expres.Router();
 
 router.get("/get-all", postController.getAll); // GET
-router.post("/create", logger, postController.create); // POST
-router.delete("/delete/:id", postController.delete); // DELETE
-router.put("/edit/:id", postController.edit); // PUT
+router.post("/create", authMiddleware, postController.create); // POST
+router.delete(
+  "/delete/:id",
+  authMiddleware,
+  authorMiddleware,
+  postController.delete
+); // DELETE
+router.put("/edit/:id", authMiddleware, authorMiddleware, postController.edit); // PUT
 router.get("/get-one/:id", postController.getOne); //GET one
 module.exports = router;
