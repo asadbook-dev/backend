53335bbb508832891075b59f1ed7fe0754a782c7
asadbek <asadbekkhudayberdiyev07@gmail.com>
2025-10-30 01:21:13 +0900

    routes changed

--- routes/post.route.js
+++ routes/post.route.js
@@ -9,6 +9,6 @@ const router = expres.Router();
 router.get("/get-all", postController.getAll); // GET
 router.post("/create", postController.create); // POST
 router.delete("/delete/:id", postController.delete); // DELETE
-router.put("/edit/:id", authMiddleware, authorMiddleware, postController.edit); // PUT
+router.put("/edit/:id", postController.edit); // PUT
 router.get("/get-one/:id", postController.getOne); //GET one
 module.exports = router;
