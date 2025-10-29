da113fd376c6dc39533261d0b039e8bcebac7e27
asadbek <asadbekkhudayberdiyev07@gmail.com>
2025-10-30 01:20:36 +0900

    edit mutation

--- client/src/components/cards/post.card.tsx
+++ client/src/components/cards/post.card.tsx
@@ -1,14 +1,58 @@
 import type { IPost } from "@/interfaces"
 import { Card, CardContent, CardFooter, CardTitle } from "../ui/card"
-import { API_URL } from "@/http"
+import $axios, { API_URL } from "@/http"
 import { Button } from "../ui/button"
 import { useConfirm } from "@/hooks/use.confirm"
+import { useForm } from "react-hook-form"
+import type z from "zod"
+import { postSchema } from "@/lib/validation"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover"
+import { useState } from "react"
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "../ui/form"
+import { Input } from "../ui/input"
+import { Textarea } from "../ui/textarea"
+import { useMutation } from "@tanstack/react-query"
+import { postStore } from "@/store/post.store"
+import { toast } from "sonner"
+import FillLoading from "../shared/fill-loading"
 
 const PostCard = ({ post }: { post: IPost }) => {
+    const [open, setOpen] = useState(false)
     const { onOpen, setPost } = useConfirm()
+    const { posts, setPosts } = postStore()
+
     const onDelete = () => {
         onOpen()
         setPost(post)
+    }
+
+    const form = useForm<z.infer<typeof postSchema>>({
+        resolver: zodResolver(postSchema),
+        defaultValues: {
+            title: post.title,
+            body: post.body
+        },
+    })
+
+    const { mutate, isPending } = useMutation({
+        mutationKey: ["edit-post"],
+        mutationFn: async (values: z.infer<typeof postSchema>) => {
+            const { data } = await $axios.put(`/post/edit/${post._id}`, values)
+
+            return data
+        },
+        onSuccess: data => {
+            const newData = posts.map(c => c._id === data._id ? data : c)
+            setPosts(newData)
+            setOpen(false)
+        },
+        onError: error => {
+            toast.error(error.message)
+        }
+    })
+    function onSubmit(values: z.infer<typeof postSchema>) {
+        mutate(values)
     }
     return (
         <Card className="pt-0">
@@ -18,9 +62,48 @@ const PostCard = ({ post }: { post: IPost }) => {
                 <p className="line-clamp-2 mt-1 text-muted-foreground text-sm ">{post.body}</p>
             </CardContent>
             <CardFooter className="gap-2">
-                <Button className="flex-1">Edit</Button>
+                <Popover open={open} onOpenChange={setOpen}>
+                    <PopoverTrigger asChild><Button className="flex-1" onClick={() => setOpen(true)}>Edit</Button></PopoverTrigger>
+                    <PopoverContent className="w-96">
+                        {isPending && <FillLoading />}
+                        <Form {...form}>
+                            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2 mt-6">
+                                <FormField
+                                    control={form.control}
+                                    name="title"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Title</FormLabel>
+                                            <FormControl>
+                                                <Input placeholder="shadcn" className="bg-secondary" disabled={isPending} {...field} />
+                                            </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                                <FormField
+                                    control={form.control}
+                                    name="body"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Body</FormLabel>
+                                            <FormControl>
+                                                <Textarea placeholder="In this article you can improve..." className="bg-secondary" disabled={isPending} {...field} />
+                                            </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                                <Button type="submit" disabled={isPending} >Submit</Button>
+                            </form>
+                        </Form>
+                    </PopoverContent>
+                </Popover>
+
                 <Button className="flex-1" variant={"destructive"} onClick={onDelete}>Delete</Button>
             </CardFooter>
+
+
         </Card>
     )
 }
